b85cacae37c8832706f59b72a1301e3ef2c7a62b
Sergey <thextreme.30@gmail.com>
2023-01-19 17:57:45 +0600

    created group models

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    members = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField(max_length=1024)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Dislike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+# class Friend(models.Model):
+#     STATUS_CHOICES = (
+#         ('pending', 'Pending'),
+#         ('accepted', 'Accepted'),
+#     )
+#     user1 = models.ForeignKey(User, related_name='user1', on_delete=models.CASCADE)
+#     user2 = models.ForeignKey(User, related_name='user2', on_delete=models.CASCADE)
+#     status = models.CharField(max_length=8, choices=STATUS_CHOICES, default='pending')
