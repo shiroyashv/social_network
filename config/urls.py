69e7f59aced0bb84e7e92b12c8224de8c8650a92
Sergey <thextreme.30@gmail.com>
2023-01-20 18:35:35 +0600

    add groups urls in main urls

--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-
-
+    path('/', include('groups.url')),
 ]
 
 if settings.DEBUG:
