c2c3e90d48a807c2498cdda26723e41e72effc10
Sergey <thextreme.30@gmail.com>
2023-01-22 23:28:34 +0600

    added urls for subscribe and like

--- groups/urls.py
+++ groups/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CommentCreateView, CommentListView, GroupCreateView, GroupDetailView, GroupListView, PostCreateView, PostDetailView, PostListView
+from .views import CommentCreateView, CommentListView, GroupCreateView, GroupDetailView, GroupListView, PostCreateView, PostDetailView, PostListView, dislike_post, like_post, subscribe_to_group, unsubscribe_from_group
 
 
 urlpatterns = [
@@ -12,4 +12,8 @@ urlpatterns = [
     path('groups/', GroupListView.as_view(), name='group_list'),
     path('group/create/', GroupCreateView.as_view(), name='create_group'),
     path('group/<slug:slug>/', GroupDetailView.as_view(), name='group_detail'),
+    path('post/<int:post_id>/like/', like_post, name='like'),
+    path('post/<int:post_id>/dislike/', dislike_post, name='dislike'),
+    path('post/<int:group_id>/subscribe/', subscribe_to_group, name='subscribe'),
+    path('post/<int:group_id>/unsubscribe/', unsubscribe_from_group, name='unsubscribe'),
 ]
